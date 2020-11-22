4fedaa4d963915cbefb163b679cd24f3b6da3cbe
Jordan Lee <leejor@ohsu.edu>
2020-11-22 11:29:01 -0800

    update scripts for new input mat, step1

--- scripts/get_fts.py
+++ scripts/get_fts.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python
 
 import pandas as pd
-import json
-import numpy as np
-import scipy.stats
-import matplotlib.pyplot as plt
-from matplotlib_venn import venn2
 
 ############# Hardcoded Values
 cancer = 'BRCA'
 pmetric = 'overall_weighted_f1'
 groups = ['gnosis', 'CF|All', 'AKLIMATE', 'nn', ['rfe15', 'fbedeBIC']]
 
-# File formatted by BK (derived from Chris Wong's master matrix, but with RFE combined)
-performance_matrix = 'src/brca_ak_sk.tsv'
-scoring_matrix = 'src/model_scoring.features'
-
-output_path = 'data/correlation/CORR_'
+file_preds = 'src/feature_list_with_performance_with_subtype_names_20200828.tsv.gz'
+file_fts = 'src/collected_features_matrix_20200722.tsv.gz'
 #############
 
-performance_df = pd.read_csv(performance_matrix,sep='\t')
+performance_df = pd.read_csv(file_preds, sep = '\t', low_memory=False)
 
 # For a group: select best model
 ct = 1
+best = []
 for group in groups:
     # all models from that group for the 3 criteria
     if type(group)== list:
@@ -30,69 +23,30 @@ for group in groups:
         subset = subset[subset['cohort'] == cancer]
         subset = subset[subset['performance_metric']== pmetric].reset_index(drop=True)
         ftID = subset.sort_values(by='Mean', ascending=False).reset_index(drop=True)['featureID'][0]
+        print('1. group: ', group)
     else:
         subset = performance_df[performance_df['feature_list_method'] == group]
         subset = subset[subset['cohort'] == cancer]
         subset = subset[subset['performance_metric']== pmetric].reset_index(drop=True)
+        print('2. group: ', group)
 
 
     # Grab the name of the model with highest MEAN performance metric
     ftID = subset.sort_values(by='Mean', ascending=False).reset_index(drop=True)['featureID'][0]
 
-    if 'AKLIMATE' in ftID: #  fix aklimate formatting issue
-        if cancer == ftID.split('_')[-1]:
-            ftID = '_'.join(ftID.split('_')[:-1])
-    if 'CF|' in ftID: # fix cloud forest mapping
-        ftID = cancer + ':' + ('_'.join(ftID.split('_')[:-1]).replace('Top_', 'Top ') )
-    if 'gnosis' in ftID: # fix gnosis
-        ftID = ':'.join([ftID.split('_')[0], ftID.split('_')[2], ftID.split('_')[1]])
-    if 'nn_jg' in ftID: # fix subscope
-        ftID = '_'.join(ftID.split('_')[:-1])
+    ##
+    # Fix naming of CloudForest (to match ft file)
+    if "CF|" in ftID:
+        ftID='Top '.join(ftID.split('Top_'))
+    ##
+
+    best.append(ftID)
     print(ftID, ' selected as best model for group')
 
-
-    # Retrieve the feature list for this particular ftID
-    with open(scoring_matrix, 'r') as fh:
-        data = json.load(fh)
+assert len(best) == 5, 'best model not found for all 5 groups'
 
 
-    if type(group) == list:
-        if 'fbed' in ftID:
-            ft_set = []
-            for k, v in data.items():
-                if 'fbed' in k:
-                    ft_set = ft_set + v
-            ft_set = list(set(ft_set))
-
-        if 'rfe' in ftID:
-            ft_set = []
-            for k, v in data.items():
-                if 'RFE' in k:
-                    ft_set = ft_set + v
-            ft_set = list(set(ft_set))
-    else:
-        ft_set = data.get(ftID)
-    print(len(ft_set), 'length of feature set')
-
-    # save output as df
-    if ct == 1: # create df if first group
-        ft_set1 = ft_set
-        ftID1 = ftID
-    elif ct == 2:
-        ft_set2 = ft_set
-        ftID2 = ftID
-    elif ct == 3:
-        ft_set3 = ft_set
-        ftID3 = ftID
-    elif ct == 4:
-        ft_set4 = ft_set
-        ftID4 = ftID
-    else:
-        ft_df = pd.DataFrame.from_dict({ftID1: ft_set1, ftID2: ft_set2, ftID3: ft_set3, ftID4: ft_set4, ftID: ft_set}, orient='index')
-#     else:
-#         ft_df[ftID]=ft_set
-    ct+=1
-
-ft_df = ft_df.T
-ft_df.to_csv(output_path + cancer + '_featurelist.tsv', sep = '\t',index=False)
-ft_df
+# Subset feature matrix for only best model per team
+ft_df = pd.read_csv(file_fts, sep = '\t', index_col=0, low_memory=False)
+ft_df = ft_df[best].drop(['feature_list_method','feature_list_cohort','feature_list_size']) # rm annotat rows
+ft_df.to_csv('data/exact_match/best_models.tsv', sep='\t')
---
+++ src/README.md
@@ -0,0 +1,67 @@
+# feature_list_with_performance_with_subtype_names_20200828.tsv.gz
+
+(syn22337110)
+
+Master feature list matrix of all group models. Created by Chris W. Downloaded from synapse on 11/22/20
+
+Pre-miRNA fix
+
+```
+This is an update to the previous file (syn22322484).
+
+This version adds datatype counts for the feature lists.
+
+The columns are:
+1 featureID
+2 cohort
+3 model
+4 performance_metric
+5 Mean
+6 Median
+7 Std
+8 StdMed
+9 SEM
+10 Count
+11 Max
+12 Min
+13 Sum
+14 feature_list_method
+15 feature_list_cohort
+16 feature_list_size
+17 subtypeID
+18 subtype_size
+19 TMP_name
+20 Cluster_name
+21 size_in_marker_paper
+22 subtypes_defined_by_mRNA
+23 subtypes_defined_by_CN
+24 subtypes_defined_by_Mutations
+25 subtypes_defined_by_Meth
+26 subtypes_defined_by_miRNA
+27 subtypes_defined_by_Histology
+28 subtypes_defined_by_iCluster
+29 subtypes_defined_by_COCA
+30 subtypes_defined_by_Other
+31 total_features
+32 GEXP_features
+33 CNVR_features
+34 METH_features
+35 MIR_features
+36 MUTA_features
+```
+
+# collected_features_matrix_20200722.tsv.gz
+
+(syn22271992)
+
+Master feature list matrix of all group models. Created by Chris W. Downloaded from synapse on 11/22/20
+
+Pre-miRNA fix
+
+```
+The current feature lists are collected in this file. Each row is a feature. Each column is a feature list. "0" or "1" indicates membership status in a feature list with "1" signifying membership. There is a special column, "total_number_of_lists", to record how many lists a feature participates in. Also, there are 3 special rows: "feature_list_method", "feature_list_cohort", "feature_list_size". The rows are sorted in descending order based on "total_number_of_lists" column.
+
+The file records the data for:
+24664 features
+1272 feature lists
+```
