d17096de26dad1932d1277918b9c97de4777b503
Jordan Lee <leejor@ohsu.edu>
2020-12-01 11:20:30 -0800

    phase 1 complete. heatmap of top per team. plot raw tarball vals

--- RUN2.sh
+++ RUN2.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/bash
 
-# requires run of RUN.sh for step 00 as input into this RUN2.sh
-#######
-# Create heatmap of feature set - for heatmap shows one model per
-#######
-## 1. Get feature_list_cohort
-# run heatmpa_topmodel.ipynb
-# 2. then run heatmap_fts.Rmd
+echo '# Purpose: Create heatmap of feature set - for heatmap shows best model per team #'
 
-
-######
-# Create heatmap of feature set - for heatmap shows best model per team
-## 1. Get feature_list_cohort
+# 0. Format feature lists of groups (best performing model)
 python scripts/get_fts.py
 echo 'completed ft list formatting'
 
-## 2. Clean file and create 2 new files
-# run pull_grp_best.ipynb
+# 1. Clean file and create 2 new files
+python scripts/pull_grp_best.py
+echo 'completed pulling group best and file cleaning'
 
-## 3. Create heatmap
-# 2. then run heatmap_fts.Rmd
+# 2. Create heatmap
+echo 'ACTION:'
+echo '-- next step: create heatmap --'
+echo '-- now manually run scripts/heatmap_fts.Rmd --'
+# then run scripts/heatmap_fts.Rmd
