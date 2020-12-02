0a7d600584a28edac0dd6cbd5585e0f30f7876dd
Jordan Lee <leejor@ohsu.edu>
2020-12-01 18:07:02 -0800

    heatmaps, tables, upset plot

--- RUN3.sh
+++ RUN3.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
+# 0. Format feature lists of groups (best performing model)
+python scripts/get_fts.py
+echo 'completed ft list formatting'
+
 # 1. Clean
 echo '-- run: notebooks/wip/step1_clean.ipynb --'
 
 # 2. Correlations
 echo '-- run: scripts/wip/corr_matrix.Rmd --'
-
