71e3aeec346ba2b784897ddae4fca8c6674127a3
Jordan Lee <leejor@ohsu.edu>
2020-12-02 16:54:20 -0800

    update RUN3 with cleanup as script

--- RUN3.sh
+++ RUN3.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/bash
 
-# 0. Format feature lists of groups (best performing model)
-python scripts/get_fts.py
-echo 'completed ft list formatting'
+# # 0. Format feature lists of groups (best performing model)
+# python scripts/get_fts.py
+# echo 'completed ft list formatting'
 
 # 1. Clean
-echo '-- run: notebooks/wip/step1_clean.ipynb --'
+python scripts/step1_clean.py \
+    -f data/heatmap/BRCA_fts_by_VALUE.tsv \
+    -o data/heatmap/BRCA_vals_meth.tsv \
+    -d N:METH
+python scripts/step1_clean.py \
+    -f data/heatmap/BRCA_fts_by_VALUE.tsv \
+    -o data/heatmap/BRCA_vals_gexp.tsv \
+    -d N:GEXP
+python scripts/step1_clean.py \
+    -f data/heatmap/BRCA_fts_by_VALUE.tsv \
+    -o data/heatmap/BRCA_vals_cnvr.tsv \
+    -d I:CNVR::
+python scripts/step1_clean.py \
+    -f data/heatmap/BRCA_fts_by_VALUE.tsv \
+    -o data/heatmap/BRCA_vals_muta.tsv \
+    -d B:MUTA:
+python scripts/step1_clean.py \
+    -f data/heatmap/BRCA_fts_by_VALUE.tsv \
+    -o data/heatmap/BRCA_vals_mir.tsv \
+    -d N:MIR::
+echo 'completed cleaning'
 
 # 2. Correlations
-echo '-- run: scripts/wip/cluster_fts_meth.Rmd --'
-echo '-- run: scripts/wip/cluster_fts_gexp.Rmd --'
-
+echo '-- run: scripts/cluster_fts_<datatype>.Rmd --'
