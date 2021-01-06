134b758784cc28b580e15b90a2a4378f758d1e88
jordan2lee <jordanl.7890@gmail.com>
2021-01-06 00:53:36 +0000

    format rfe and fbed results - todo test this

--- scripts/synapse.py
+++ scripts/synapse.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-
 # Purpose convert file to group format for synapse
+
+import argparse
+
 ########## hardcoded
 team = 'skgrid'
-ft_method = 'fbedeBIC'
+list_ft_methods = ['fbedeBIC', 'rfe15']
 cancer_list = [
     "ACC",
     "BLCA",
@@ -35,9 +37,15 @@ cancer_list = [
 ##########
 
 #####
-# Function
+# Functions
 #####
-def parse_combined_file(input_file):
+def get_arguments():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument("-o", "--outfile", help ="output file name", required=True, type=str)
+    return parser.parse_args()
+
+
+def parse_combined_file(input_file, ft_method):
     with open(input_file, 'r') as fhc:
         header = True
         ftset = []
@@ -45,26 +53,20 @@ def parse_combined_file(input_file):
             if header == False:
                 # FtsetID
                 fID = '_'.join([team, tumor, ''.join([ft_method, iteration] ) ])
-
                 # cancer cohort
                 cohort = '[\"'+ tumor + '\"]'
-
                 # ft set
                 ft = line.strip().split('\t')[1]
                 ftset.append(ft)
-
-
             else:
                 header = False
-
         # clean up list of fts
         ftset = str(ftset).replace("\'", "\"").replace(' ', '')
-
         # write output results
         out.write('\t'.join([fID, cohort, ftset]) + '\n')
 
 
-def parse_per_file(file):
+def parse_per_file(file, ft_method):
     ftset = []
     with open(file, 'r') as fh:
         header = True
@@ -72,10 +74,8 @@ def parse_per_file(file):
             if header == False:
                 # FtsetID
                 fID = '_'.join([team, tumor, ''.join([ft_method, iteration] ) ])
-
                 # cancer cohort
                 cohort = '[\"'+ tumor + '\"]'
-
                 # ft set
                 line = line.strip().split('\t')[1:]
                 for s in line:
@@ -83,10 +83,8 @@ def parse_per_file(file):
                         ftset.append(s)
             else:
                 header = False
-
     # clean up list of fts
     ftset = str(ftset).replace("\'", "\"").replace(' ', '')
-
     # write output results
     out.write('\t'.join([fID, cohort, ftset]) + '\n')
 
@@ -94,19 +92,21 @@ def parse_per_file(file):
 ####
 # Main
 ####
-file_out = 'data/ft_selection_skgrid/{}_{}.tsv'.format(ft_method, '010520')
+args = get_arguments()
+file_out = args.outfile
+
 with open(file_out, 'w') as out:
     out.write('Feature_Set_ID\tTCGA_Projects\tFeatures\n')
 
-    # For each cancer pull the feature list
-    for tumor in cancer_list:
-        # for the combined file
-        iteration = 'combined'
-        file = 'data/ft_selection_skgrid/{}/{}_{}--combined_platform.tsv'.format(tumor, tumor, ft_method)
-        parse_combined_file(file)
-
-
-        # for the per platform file
-        iteration = 'perplatform'
-        file = 'data/ft_selection_skgrid/{}/{}_{}--per_platform.tsv'.format(tumor, tumor, ft_method)
-        parse_per_file(file)
+    # For each feature selection model
+    for ft_method in list_ft_methods:
+        # For each cancer pull the feature list
+        for tumor in cancer_list:
+            # For the combined file
+            iteration = 'combined'
+            file = 'data/ft_selection_skgrid/{}/{}_{}--combined_platform.tsv'.format(tumor, tumor, ft_method)
+            parse_combined_file(file, ft_method)
+            # For the per platform file
+            iteration = 'perplatform'
+            file = 'data/ft_selection_skgrid/{}/{}_{}--per_platform.tsv'.format(tumor, tumor, ft_method)
+            parse_per_file(file, ft_method)
