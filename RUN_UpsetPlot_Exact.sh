55d4ff1ea26981939f34766057609453593b7b78
Jordan Lee <leejor@ohsu.edu>
2021-01-20 17:37:05 -0800

    missed comment/uncomment

--- RUN_UpsetPlot_Exact.sh
+++ RUN_UpsetPlot_Exact.sh
@@ -2,14 +2,14 @@
 
 tumor_cohort='BRCA'
 #
-# # 0. Format feature lists of groups (best performing model)
-# python scripts/get_fts.py \
-#     --tumor ${tumor_cohort} \
-#     -m overall_weighted_f1 \
-#     -f1 src/collected_features_matrix_20200722.tsv.gz \
-#     -f2 src/feature_list_with_performance_with_subtype_names_20200828.tsv.gz \
-#     --out data/figure_panel_a/best_models_${tumor_cohort}.tsv
-# echo 'completed ft list formatting'
+# 0. Format feature lists of groups (best performing model)
+python scripts/get_fts.py \
+    --tumor ${tumor_cohort} \
+    -m overall_weighted_f1 \
+    -f1 src/collected_features_matrix_20200722.tsv.gz \
+    -f2 src/feature_list_with_performance_with_subtype_names_20200828.tsv.gz \
+    --out data/figure_panel_a/best_models_${tumor_cohort}.tsv
+echo 'completed ft list formatting'
 
 # 1. Exact Feature Match - Overlaps
 # Create upset plots
