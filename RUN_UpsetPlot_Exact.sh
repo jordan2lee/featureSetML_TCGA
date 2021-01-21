dbeab944bba59f4e234b2cf9e89edfbfc2b76756
Jordan Lee <leejor@ohsu.edu>
2021-01-20 17:36:44 -0800

    team order matches btwn heatmap and upset plot

--- RUN_UpsetPlot_Exact.sh
+++ RUN_UpsetPlot_Exact.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/bash
 
 tumor_cohort='BRCA'
-
-# 0. Format feature lists of groups (best performing model)
-python scripts/get_fts.py \
-    --tumor ${tumor_cohort} \
-    -m overall_weighted_f1 \
-    -f1 src/collected_features_matrix_20200722.tsv.gz \
-    -f2 src/feature_list_with_performance_with_subtype_names_20200828.tsv.gz \
-    --out data/figure_panel_a/best_models_${tumor_cohort}.tsv
-echo 'completed ft list formatting'
+#
+# # 0. Format feature lists of groups (best performing model)
+# python scripts/get_fts.py \
+#     --tumor ${tumor_cohort} \
+#     -m overall_weighted_f1 \
+#     -f1 src/collected_features_matrix_20200722.tsv.gz \
+#     -f2 src/feature_list_with_performance_with_subtype_names_20200828.tsv.gz \
+#     --out data/figure_panel_a/best_models_${tumor_cohort}.tsv
+# echo 'completed ft list formatting'
 
 # 1. Exact Feature Match - Overlaps
 # Create upset plots
+# Note that --headers must match order of --infile headers
 Rscript scripts/upset_exactMatch.R \
     -c ${tumor_cohort} -m distinct --infile data/figure_panel_a/best_models_${tumor_cohort}.tsv \
     --outdir data/figure_panel_a --outname upsetplot_${tumor_cohort}.pdf \
-    --headers Gnosis,CloudForest,AKLIMATE,SubSCOPE,SciKitGrid
+    --headers JADBIO,CForest,AKLIMATE,SubSCOPE,SKGrid
 echo 'completed upset plot - mode distinct'
