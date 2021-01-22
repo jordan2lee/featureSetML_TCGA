4fd4c7627cad87c566d7603fdbc5fb3140a95eaf
Jordan Lee <leejor@ohsu.edu>
2021-01-22 13:28:47 -0800

    RUN_Heatmap_Exact updated to commandline call which cancer, ran fir st half of heatmap pipeline on 3 cancers

--- RUN_Heatmap_Exact.sh
+++ RUN_Heatmap_Exact.sh
@@ -2,19 +2,16 @@
 
 echo 'Purpose: Create heatmap of feature set - for heatmap shows best model per team'
 
-tumor_cohort='BRCA'
+tumor_cohort=${1}
 
-# # 1. Clean file and create 2 new files
-# python scripts/pull_grp_best.py \
-#     --tumor ${tumor_cohort} \
-#     -f1 data/figure_panel_a/best_models_${tumor_cohort}.tsv \
-#     -f2 src/tarball/${tumor_cohort}_v8_20200203.tsv \
-#     --out1 data/figure_panel_b/${tumor_cohort}_fts_by_TEAM.tsv \
-#     --out2 data/figure_panel_b/${tumor_cohort}_fts_by_VALUE.tsv
-# echo 'completed pulling group best and file cleaning'
-
-# 2. Extract hallmark gene sets
-Rscript scripts/extract_hallmarks.R
+# 1. Clean file and create 2 new files
+python scripts/pull_grp_best.py \
+    --tumor ${tumor_cohort} \
+    -f1 data/figure_panel_a/best_models_${tumor_cohort}.tsv \
+    -f2 src/tarball/${tumor_cohort}_v9_20201029.tsv \
+    --out1 data/figure_panel_b/${tumor_cohort}_fts_by_TEAM.tsv \
+    --out2 data/figure_panel_b/${tumor_cohort}_fts_by_VALUE.tsv
+echo 'completed pulling group best and file cleaning'
 
 # 2. Create heatmap
 # then run scripts/heatmap_fts.Rmd
