02e695ffdbba2a06052b9af32e613fb292468fbb
Jordan Lee <leejor@ohsu.edu>
2021-02-19 11:35:02 -0800

    cleaner file names, fix typo in get_fts max ft set size

--- RUN_overlap.sh
+++ RUN_overlap.sh
@@ -7,7 +7,7 @@ tumor_cohort=${1}
 python scripts/get_fts.py \
     --tumor ${tumor_cohort} \
     -m overall_weighted_f1 \
-    --filters none \
+    --filters 1000 \
     -f1 src/collected_features_matrix_20200722.tsv.gz \
     -f2 src/feature_list_with_performance_with_subtype_names_20200828.tsv.gz \
     --out data/figure_panel_a/best_models_${tumor_cohort}.tsv
