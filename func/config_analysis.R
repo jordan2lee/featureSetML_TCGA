27a01947e0ad473fd935879757e891bd01945d6e
Jordan Lee <leejor@ohsu.edu>
2021-06-07 14:43:35 -0700

    rename config func

--- func/config_analysis.R
+++ func/config_analysis.R
@@ -158,3 +158,14 @@ platform_display_text <- function(platform){
   )
   return(l1[[platform]])
 }
+
+ht_matrix_colors <- function(platform){
+  l1 <- list(
+    'CNVR' = structure(c('blue', 'white', 'red'), names = c(-1, 0, 1)),
+    'MIR' = colorRamp2(c(-2, 0, 2), c('blue', 'white', 'red')),
+    'METH' = NULL,
+    'GEXP' = colorRamp2(c(-2, 0, 2), c('blue', 'white', 'red')),
+    'MUTA' = structure(c('blue','red'), names = c(0, 1))
+  )
+  return(l1[[platform]])
+}
