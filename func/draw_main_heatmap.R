6179d724a4d6a3d5dbc5ff66379e2a53f559435a
Jordan Lee <leejor@ohsu.edu>
2021-06-07 15:04:17 -0700

    update brca gexp to be a function

--- func/draw_main_heatmap.R
+++ func/draw_main_heatmap.R
@@ -6,40 +6,21 @@ get_main_heatmap <- function(plat, ht_name, cancer){
 
   # Draw
   if (cancer == 'BRCA' && plat == 'GEXP'){
-    colnames(mat2) <- ft2gene_gexp(colnames(mat2)) # full ft to gene symbol only
+    # Quick abbrev of feature gene symbols
+    colnames(mat2) <- ft2gene_gexp(colnames(mat2))
+    # Logging
     print('#### FEATURES:')
     print(colnames(mat2))
-
-    symb_colors <- color_pam(in_pam)
-    fig <- Heatmap(
+    # Create final heatmap
+    fig <- brca_gexp(
+      cancer,
+      plat,
       mat2,
-      name = ht_name,
-      cluster_rows = FALSE,
-      cluster_columns = FALSE,
-      show_row_names = FALSE,
-      show_column_names = args$show_features,
-      column_names_gp = gpar(col = symb_colors),
-      column_title = col_title,
-      column_title_gp = gpar(
-        col = get_colors_platform(plat),
-        fontface = 'bold',
-        fontfamily = get_gpar('font_fam'),
-        fontsize = get_gpar('axis_size')
-      ),
-      row_title = paste('Samples (n=', ht_rows, ')', sep=''),
-      row_title_gp = gpar(
-        fontface = 'bold',
-        fontfamily = get_gpar('font_fam'),
-        fontsize = get_gpar('axis_size')
-      ),
-      right_annotation = subtype_ha,
-      # bottom_annotation = col_annot,
-      bottom_annotation = dev_bottom_annot(paste(cancer, plat, sep='_'), in_pam),
-      row_title_side = "right",
-      use_raster = TRUE,
-      na_col = 'white',
-      col = legend_colors,
-      heatmap_legend_param = legend_l_param
+      ht_name,
+      subtype_ha,
+      ht_matrix_colors(plat),
+      ht_combined_legend(plat, main_ht_name),
+      color_pam(in_pam)
     )
   } else if (plat == 'GEXP'){
     # temp fix to handle error for when only 1 ft (SKCM)
