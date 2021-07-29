d1d2bb625d3db3a05ee2faba4ea5a4076681a20f
jordan2lee <jordanl.7890@gmail.com>
2021-07-29 14:38:46 -0700

    better colors of subtype legend; better color gradients for ft importances for each team

--- func/get_base_heatmap.R
+++ func/get_base_heatmap.R
@@ -112,7 +112,7 @@ get_base_heatmap <- function(prefix, cancer, header_jadbio, header_cforest, head
         "Cloud Forest" = c('0' = "#333333", '1' = "#BFBFFF"),
         "AKLIMATE\nmin-max" = c('0' = "#333333", '1' = "#BFFEFF"),
         "SubSCOPE" = c('0' = "#333333", '1' = "#AEFEB0"),
-        "SciKitGrid" = c('0' = "#333333", '1' = "#FCC0BF")
+        "SciKitGrid" = c('0' = "#333333", '1' = "#D23A71")
       ),
       show_legend = c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE),
       annotation_name_gp= gpar(fontsize = 8),
