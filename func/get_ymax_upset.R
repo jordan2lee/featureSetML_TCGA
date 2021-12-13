41dfe777593d86dca4a68b45f7cc4a7b5ea937fb
jordan2lee <jordanl.7890@gmail.com>
2021-12-13 11:30:10 -0800

    add liver to list

--- func/get_ymax_upset.R
+++ func/get_ymax_upset.R
@@ -4,7 +4,7 @@ get_ymax_upset <- function(cancer){
     'ACC' = 110, 'BLCA' = 70, 'BRCA' = 70,
     'CESC'= 60, 'COADREAD' = 50, 'ESCC' = 80,
     'GEA' = 50, 'HNSC' = 60, 'KIRCKICH' = 70, 'KIRP' = 80,
-    'LGGGBM' = 60, 'LUAD' = 80, 'LUSC' = 60,
+    'LGGGBM' = 60, 'LIHCCHOL' = 80, 'LUAD' = 80, 'LUSC' = 60,
     'MESO' = 90, 'OV' = 70, 'PAAD' = 30,
     'PCPG' = 60, 'PRAD' = 80, 'SARC' = 70,
     'SKCM' = 11.5, 'TGCT' = 70, 'THCA' = 70,
